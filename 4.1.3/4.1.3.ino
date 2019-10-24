6c9ef394dd00f4f091dbe85fee224fba8bb38ccf
Yupeng Li <yupengli@seas.upenn.edu>
2019-10-24 14:44:19 -0400

    most recent sender code

--- 4.1.3/4.1.3.ino
+++ 4.1.3/4.1.3.ino
@@ -8,19 +8,39 @@ const char* password = "2513";
 IPAddress myIPaddress(192,168,1,142);
 const int UDP_PACKET_SIZE = 100;
 byte packetBuffer[UDP_PACKET_SIZE+1];
-
-WiFiUDP UDPTestServer
+void handleUDPServer();
+WiFiUDP UDPTestServer;
 void setup() {
   // put your setup code here, to run once:
-WiFi.config(myIPAdress,IPAddress(192,168,1,1),IPAddress(255,255,255,0));
-WiFi.begin(ssid,password);
+WiFi.config(myIPaddress,IPAddress(192,168,1,1),IPAddress(255,255,255,0));
 Serial.begin(115200);
-pinMode(2,OUTPUT);
-Serial.print("Setting Access Point...");
+WiFi.begin(ssid,password);
+
+UDPTestServer.begin(2808);
+
+while (WiFi.status() != WL_CONNECTED){
+  delay(500);
+  Serial.print(".");
+}
+
+Serial.println("WiFi connected");
+packetBuffer[UDP_PACKET_SIZE] = 0;
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+handleUDPServer();
+delay(1);
+}
 
+void handleUDPServer()
+{
+  int cb = UDPTestServer.parsePacket();
+  if (cb)
+  {
+    UDPTestServer.read(packetBuffer, UDP_PACKET_SIZE);
+    Serial.printf("%s\n",packetBuffer);
+  }
 }
