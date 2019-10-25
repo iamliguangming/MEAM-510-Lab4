17e07752d0be7ced36de5db115f69123a31b0a47
Yupeng Li <yupengli@seas.upenn.edu>
2019-10-25 17:20:34 -0400

    Its not working right now!

--- Station_receving/Station_receving.ino
+++ Station_receving/Station_receving.ino
@@ -1,8 +1,13 @@
-#include <WiFi.h>
-#include <WiFiUdp.h>
+#include <WiFi.h> //include the wifi file
+#include <WiFiUdp.h>//include the UDP protocal file
 
-const char* ssid = "liguangming";
-const char* password = "Iamlegend1";
+const char* ssid = "Bozhou_Zha";
+const char* password = "19970524";
+const int POTPin = 32;
+const int LED = 0;
+const int Switch = 33;
+int val = 0;
+int j = 0 ;
 void handleUDPServer();
 void fncUdpSend();
 void fncUdpSendint(int i);
@@ -11,21 +16,23 @@ WiFiUDP UDPTestServer;
 const int UDP_PACKET_SIZE = 100;
 char udpBuffer[UDP_PACKET_SIZE];
 byte packetBuffer[UDP_PACKET_SIZE+1];
-IPAddress ipTarget(10,0,0,201);
-IPAddress myIP(10,0,0,233);
+IPAddress ipTarget(192,168,1,191);
+IPAddress myIP(192,168,1,142);
 //WiFiServer server(80);
 
 
 void setup() {
+pinMode(Switch,INPUT);
 pinMode(2,OUTPUT);
+pinMode(LED,OUTPUT);
 Serial.begin(115200);
 Serial.print("Connecting to ");
 Serial.println(ssid);
 WiFi.mode(WIFI_STA);
-WiFi.config(myIP,IPAddress(10,0,0,1),IPAddress(255,255,255,0));
+WiFi.config(myIP,IPAddress(192,168,1,1),IPAddress(255,255,255,0));
 WiFi.begin(ssid,password);
 //server.begin();
-UDPTestServer.begin(208);
+UDPTestServer.begin(2808);
 
 while (WiFi.status() != WL_CONNECTED)
 {
@@ -43,16 +50,30 @@ packetBuffer[UDP_PACKET_SIZE] = 0 ;
 void loop() {
   // put your main code here, to run repeatedly:
 
-
+Serial.println(val);
 handleUDPServer();
 delay(1);
 fncUdpSend();
-fncUdpSendint(12345);
-handleUDPServerint();
-digitalWrite(2,HIGH);
-delay(1000);
-digitalWrite(2,LOW);
-delay(1000);
+//fncUdpSendint(12345);
+//handleUDPServerint();
+while (j  <= 49)
+{
+val = map(analogRead(POTPin),0,4095,100,10000);
+Serial.print(!digitalRead(Switch));
+digitalWrite(LED,HIGH);
+delay(val/100);
+j=j+1;
+}
+j=0;
+while ( j <=49)
+{
+val = map(analogRead(POTPin),0,4095,100,10000);
+Serial.print(!digitalRead(Switch));
+digitalWrite(LED,LOW);
+delay(val/100);
+j=j+1;
+}
+j = 0;
 }
 
 void handleUDPServer(){
@@ -65,8 +86,8 @@ void handleUDPServer(){
 
 void fncUdpSend()
 {
-  strcpy(udpBuffer,"hello world");
-  UDPTestServer.beginPacket(ipTarget, 209);
+  strcpy(udpBuffer,"Zhageshuaiye!");
+  UDPTestServer.beginPacket(ipTarget, 2808);
   UDPTestServer.printf("%s",udpBuffer);
   UDPTestServer.endPacket();
   Serial.println(udpBuffer);
@@ -76,14 +97,14 @@ void fncUdpSendint(int i)
   udpBuffer[0] = i & 0xff;
   udpBuffer[1] = i >> 8 ;
   udpBuffer[2] = 0;
-  UDPTestServer.beginPacket(ipTarget, 209);
+  UDPTestServer.beginPacket(ipTarget, 2808);
   UDPTestServer.printf("%s",udpBuffer);
   UDPTestServer.endPacket();
 }
 void handleUDPServerint()
 {
   int i, cb = UDPTestServer.parsePacket();
-  if (cb) 
+  if (cb)
   {
     UDPTestServer.read(packetBuffer,UDP_PACKET_SIZE);
     i = packetBuffer[0] + packetBuffer[1] << 8;
