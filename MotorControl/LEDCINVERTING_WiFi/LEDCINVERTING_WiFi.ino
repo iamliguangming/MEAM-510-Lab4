9d93ee7547481976d2a3b32b73725895379c0979
Yupeng Li <yupengli@seas.upenn.edu>
2019-10-31 23:14:20 -0400

    ?!!?!?!?!??!?!?

--- MotorControl/LEDCINVERTING_WiFi/LEDCINVERTING_WiFi.ino
+++ MotorControl/LEDCINVERTING_WiFi/LEDCINVERTING_WiFi.ino
@@ -2,9 +2,11 @@
 #include <WiFiUdp.h>;
 int port = 1609;
 int cb = 0;
+int val = 0;
+void receivePacket();
 int MessageReceived = 0;
 const char* ssid = "NANI";
-WiFiUDP = UDPTestServer;
+WiFiUDP  UDPTestServer;
 IPAddress myIPaddress(192,168,1,142);
 const int UDP_PACKET_SIZE = 100;
 byte packetBuffer[UDP_PACKET_SIZE + 1];
@@ -26,6 +28,19 @@ const int FrontorBack = 33;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200);
+Serial.println();
+Serial.print("Connecting to");
+Serial.println(ssid);
+WiFi.mode(WIFI_STA);
+WiFi.config(myIPaddress,IPAddress(192,168,1,1),IPAddress(255,255,255,0));
+WiFi.begin(ssid);
+while (WiFi.status()!=WL_CONNECTED);
+{
+  delay(500);
+  Serial.print(".");
+}
+Serial.println("WiFi Connected ");
+packetBuffer[UDP_PACKET_SIZE] = 0;
 ledcSetup(LEDC_CHANNEL,LEDC_FREQ_HZ,LEDC_RESOLUTION_BITS);
 ledcSetup(LEDC_CHANNEL1,LEDC_FREQ_HZ,LEDC_RESOLUTION_BITS);
 ledcAttachPin(Enable,LEDC_CHANNEL);
@@ -34,11 +49,12 @@ pinMode(A1,OUTPUT);
 pinMode(A44,OUTPUT);
 pinMode(EnableControl,INPUT);
 pinMode(FrontorBack,INPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int val = map(analogRead(EnableControl),0,4095,-1000,1000);
+  receivePacket();
   uint32_t duty = LEDC_RESOLUTION*abs(val)/1000;
   Serial.println(val);
   if (val >= 0){
@@ -48,12 +64,21 @@ void loop() {
   }
   else if (val<0)
   {
-//  duty = LEDC_RESOLUTION*(10\
-00-val)/1000;
+//  duty = LEDC_RESOLUTION*(1000-val)/1000;
   digitalWrite(A1,LOW);
   digitalWrite(A44,LOW);
   }
   ledcWrite(LEDC_CHANNEL,duty);
   ledcWrite(LEDC_CHANNEL1,duty);
+  cb = 0;
+}
 
+void receivePacket(){
+  cb = UDPTestServer.parsePacket();
+  if (cb){
+    UDPTestServer.read(packetBuffer,UDP_PACKET_SIZE);
+    val = (packetBuffer[1]<<8 |packetBuffer[0]);
+    Serial.print(val);
+
+  }
 }
---
+++ WiFi module/WiFi module.ino
@@ -0,0 +1,94 @@
+#define POT 32 // setting up the pot, led, and switch to the GPIO pins that do not get affected in WIFI
+
+#include <WiFi.h> //libraries
+#include <WiFiUdp.h>
+
+int potread; // initializing the reading from the pot that has been mapped
+int cb = 0; // the size of the packet received. 0 if no packet received
+int port = 1609; // my local port
+int targetPort = 1609; // other player's port
+int MessageSent = 0;
+//both for send and receive
+const char* ssid = "NANI"; // my wifi name since I am in AP mode
+//const char* password = "GDI";
+
+//WiFiServer server(port);
+
+//for receiver
+WiFiUDP UDPTestServer; // setting up UDP communication for receiver
+IPAddress myIPaddress (192, 168, 1, 125); //my IP address
+
+const int UDP_PACKET_SIZE = 100; // can be up to 65535
+byte packetBuffer [UDP_PACKET_SIZE + 1]; // this will hold the other player's packet
+
+//for sender
+WiFiUDP udp; // settinng up UDP communication for sending to the other player
+//const int UDP_PACKET_SIZE= 100;
+char udpBuffer[UDP_PACKET_SIZE]; // this will hold the packet that I am sending
+IPAddress ipTarget(192, 168, 1, 142); //partner's IP address.
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); // baud rate
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  pinMode(POT, INPUT); // setting up the pot, leds, and switch
+  pinMode(2, OUTPUT);
+//
+//  //station mode
+//  WiFi.mode(WIFI_STA);
+//  WiFi.config(myIPaddress, IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+//  WiFi.begin(ssid);
+
+    //AP mode
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid);
+    delay(100);
+    WiFi.softAPConfig(myIPaddress, IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+
+  udp.begin(port); // starting the udp communications
+  UDPTestServer.begin(port);
+//
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  Serial.println("WiFi connected");
+  packetBuffer[UDP_PACKET_SIZE] = 0; // null terminate
+
+}
+
+void loop() {
+  MessageSent = map(analogRead(POT),0,4095,-1000,1000);
+  sendPacket();
+
+
+
+}
+
+void receivePacket() { // FOR RECEIVING
+  cb = UDPTestServer.parsePacket(); // parse packet. Sees if I get something
+  if(cb){ // If I did
+    UDPTestServer.read(packetBuffer, UDP_PACKET_SIZE); // read the packet, though it is in bytes
+//    Serial.printf("%s\n", packetBuffer);
+//    int first = (int)(packetBuffer[0]);
+//    int second = (int)(packetBuffer[1]<<8);
+    got = (packetBuffer[1]<<8) | packetBuffer[0]; // convert it to an integer
+    Serial.println(got);
+//    cb = 0;
+  }
+}
+
+void sendPacket() {
+//  char buf[8];
+//  itoa(tenth, buf, 10);
+//  strcpy(udpBuffer, buf);// send what ever you want upto buffer size
+  udpBuffer[0] = MessageSent & 0xff; // send LSB
+  udpBuffer[1] = MessageSent >> 8; // send MSB
+  udpBuffer[2] = 0; // null terminate
+  udp.beginPacket(ipTarget, targetPort);  // send to opponent port
+  udp.printf("%s", udpBuffer);
+  udp.endPacket(); // end msg
+  delay(100); // used to prevent multiple msgs from a long press of a button.
+}
