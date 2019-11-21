df5a97c2dd40a60a21be1bf5ac02ce0f13ed77f2
Yupeng Li <yupengli@seas.upenn.edu>
2019-11-20 20:30:19 -0500

    Car code combined with vive base station

--- MotorControl/Car/Car.ino
+++ MotorControl/Car/Car.ino
@@ -30,7 +30,77 @@ const int N_A1 =4;//4A used to control the NOT direction on H bridge
 const int Enable=22;//Enable pin used to control the PWM on H bridge 1
 const int Enable1 = 0;//Enable pin used to control PWM on H bridge 2
 const int ServoControl = 33;//Servo control pin used to control the PWM for the servo
+const int PhotoDiode = 27;
+//--------------------------------------------------------------------------------------
+//The following are the constants used for the VIVE sensing
+int prevT =0;
+int currT = 0;
+int flagx = 1;
+int flagy = 0;
+int start = 0 ;
+int counter = 0;
+int timediff = 0;
+//The end for constants used for VIVE sensing
+//--------------------------------------------------------------------------------------
+//The following function is stored in RAM for interrupt
+void IRAM_ATTR calcT(){ // interrupt function
+  if(digitalRead(PhotoDiode) == LOW){ // This checks for pulse width
+    currT = micros(); // finds time in microseconds
+    timediff = currT - prevT; // calculate time of the pulse width
+    if(timediff < 60){ // if the width is less than 60, which is less than the pulse width of the sync pulse, but larger than the x/y pulses
+      if(flagx){ // if the x flag is on
+        Serial.print("x-pulse: "); // print out the pulse value
+        Serial.println(timediff);
+        delayMicroseconds(1);
+        flagx = 0; // set the flag to 0 so that we dont read the next pulse as x
+        flagy = 1; // turn this flag on to read the next pulse as y
+      }
+      else if(flagy){ // if x flag isnt on but y flag is on
+        Serial.print("  y-pulse: "); // print out the pulse value
+        Serial.println(timediff);
+        delayMicroseconds(1);
+//        flagx = 1;
+        flagy = 0; // reset the flag back to 0. At this point, both the x and y flags are off.
+      }
+      counter = 0; // reset the counter. This is mainly for the else statement where we look at the counter value and the flags on line 49 (if nothing changes)
+    }
+    else{
+//      Serial.println("im counting up");
+      counter++; // increments the counter
+//      Serial.println(counter);
+      if(counter == 3){ // if I have counted 3 sync pulses, then I know the next pulse has to be an x pulse.
+        flagx = 1; // because we know the next one is an x pulse, we can safely set the x flag on and prepare for the next pulse.
+        counter = 1; // reset the counter to 1. This time we did not set it to 0 because we need to calculate the x time distance, which requires the counter to be 1 (as seen in the else statement below)
+//        Serial.println(counter);
+      }
+    }
+    start = prevT; // sets the start time to the rising edge of the pulse. This helps find the x/y time distance
+    prevT = currT; // resets the previous time to the current time to be used in finding the pulse width
+  }
+  else{ // Here we are looking at the rising edge.
+//    Serial.println("im in else");
+    currT = micros(); // finds the time in microseconds
+    timediff = currT - start; //calculate the time difference
+    if(counter == 1 && (flagx || flagy)){ // checks whether the counter is 1 and if either flag is on. The counter is always on before the x and y pulses, as well as after the y pulse; however, we prevent the issue of counting the sync pulse after the y pulse by checking the flags, which are both 0 after the y pulse.
+      if(flagx){ // checks the x flag. If x flag is on here, then we are on the x pulse. Else, on the y pulse.
+        Serial.print("    x time:"); // print the x time distance
+        Serial.println(timediff);
+        delayMicroseconds(1);
+      }
+      else{ // on y pulse
+        Serial.print("      y time:"); // print the y time distance
+        Serial.println(timediff);
+        delayMicroseconds(1);
+      }
+    }
+    prevT = currT; // set the previous time to current time (on rising edge) for pulse width.
+  }
+}
+//The end of the interrupt function for VIVE sensing
+//--------------------------------------------------------------------------------------
 
+
+//The following is the setup function which only runs once
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200);//Setup Serial Communication at 115200(For debug)
@@ -67,8 +137,13 @@ pinMode(A1,OUTPUT);//Set 1A as output
 pinMode(N_A1,OUTPUT);//Set 4A as output
 // pinMode(EnableControl,INPUT);
 pinMode(ServoControl,OUTPUT);//Set ServoControl as output
+pinMode(PhotoDiode,INPUT);//Set the PhotoDiode pin as an input pin
+attachInterrupt(digitalPinToInterrupt(PhotoDiode),calcT,CHANGE);//make the interrupt
 
 }
+
+//The end of the setup function
+//--------------------------------------------------------------------------------------
 
 void loop() {
   // put your main code here, to run repeatedly:
